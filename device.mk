d2db482ab71d45466d612d435883bc7619d174c7
adi8900 <adrianszymanski242@gmail.com>
2020-02-22 12:58:08 +0100

    davinci: get rid of overlay-lineage
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -14,8 +14,7 @@ TARGET_PRODUCT_HARDWARE := davinci
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
