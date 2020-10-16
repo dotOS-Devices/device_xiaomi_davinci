4a965d19f81e3a3509f8a4948664792494748c53
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-10-16 19:01:42 +0200

    davinci: manually call gapps for now * only if WITH_GAPPS flag is set to true Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,12 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
 
+# Gapps
+ifeq ($(WITH_GAPPS), true)
+    $(call inherit-product, vendor/gapps/gapps.mk)
+    DOT_BUILD_TYPE:=GAPPS
+endif
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
