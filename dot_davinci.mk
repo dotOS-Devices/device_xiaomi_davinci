2657ff596b05fc37c6cfdc82074b3fa64a056e69
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-02-08 16:05:20 +0100

    davinci: enable blur via custom flag
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- dot_davinci.mk
+++ dot_davinci.mk
@@ -25,6 +25,7 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 $(call inherit-product, vendor/dot/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 EXTRA_FOD_ANIMATIONS := true
+TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 
