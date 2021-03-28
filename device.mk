475aad43909138fbaa074f6f2f36c9a5f335e1c3
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-03-28 09:58:56 +0000

    [dnm] davinci: audio: back to oss audio hal * tfa98xx amplifier is now implemented rom side
    Revert "davinci: don't build oss audio hal"
    
    This reverts commit 788d6e5e4173f8fe5021335d88cb110321f1e8e1.
    
    Revert "davinci: proprietary: Switch to prebuilt audio hal"
    
    This reverts commit 1daa5a9cf2e8065d31b5dffb2f62dd2caba10562.
    
    Revert "davinci: audio: Import audio configs from miui_DAVINCI_21.3.3_e32787bfb0_11.0"
    
    This reverts commit 2036905964f0679dd4d59f3f3ff233f570db44da.

--- device.mk
+++ device.mk
@@ -50,21 +50,36 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.bluetooth.default \
+    audio.primary.sm6150 \
     audio.usb.default \
     audio.r_submix.default
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libhdmiedid \
+    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libsndmonitor \
+    libspkrprot \
+    libvolumelistener
 
 PRODUCT_PACKAGES += \
     libaudio-resampler \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
